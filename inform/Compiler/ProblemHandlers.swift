6988877fe21c701e350ad0f7c1551f45d22070d5
C.W. Betts <computers57@hotmail.com>
2021-11-27 00:03:41 -0700

    Consolidate the Compiler Problem classes into one file

--- inform/Compiler/ProblemHandlers.swift
+++ inform/Compiler/ProblemHandlers.swift
@@ -56,3 +56,36 @@ class CBlorbProblem: NSObject, IFCompilerProblemHandler {
 		return URL(string: "inform:/GoodCblorb.html")
 	}
 }
+
+/// Class that deals with problems with the Inform 6 stage of a Natural Inform build process
+class Inform6Problem: NSObject, IFCompilerProblemHandler {
+	func urlForProblem(errorCode: Int32) -> URL? {
+		return URL(string: "inform:/ErrorI6.html")
+	}
+}
+
+/// Class that deals with problems with the Natural Inform compiler
+class NaturalProblem: NSObject, IFCompilerProblemHandler {
+	func urlForProblem(errorCode: Int32) -> URL? {
+		// Code 0 indicates compiler succeeded
+		// Code 1 indicates a 'normal' failure
+		// We ignore negative return codes should they occur
+		guard errorCode > 1 else {
+			return nil
+		}
+		
+		// Default error page is Error0
+		var fileURLString = "inform:/Error0.html"
+		
+		// See if we've got a file for this specific error code
+		let specificFile = String(format: "Error%i", errorCode)
+		let resourcePath = Bundle.main.path(forResource: specificFile, ofType: "html")
+		
+		if let resourcePath = resourcePath, FileManager.default.fileExists(atPath: resourcePath) {
+			fileURLString = String(format: "inform:/%@.html", specificFile)
+		}
+		
+		// Return the result
+		return URL(string: fileURLString)
+	}
+}
